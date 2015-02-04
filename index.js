6e443972d50fd52499e589f4c24546709567b99a
Alex Mizrahi <alex.mizrahi@gmail.com>
2015-02-05 00:11:09 +0200

    added callback

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var request = require('request');
 
 exports.url = "http://datacoll.chromapass.net/";
 
-exports.log = function (coll, data) {
-  request({method: "POST", url: exports.url + coll, json: true,
-      body: data}, function () {});
+exports.log = function (coll, data, cb) {
+  if (!cb) cb = function () {};
+  request({method: "POST", url: exports.url + coll,  json:true,
+    body: data}, function (err, response, body) {
+      if (err) cb(err);
+      else if (response.statusCode != 200) {
+        cb(new Error("log satus code: " + response.statusCode));
+      } else {
+        cb(null, body);
+      }
+    });
 }
 
